cf56b0e6f626649a1bbec9da2e93472148eede61
Matt Carvin <90224411+mcarvin8@users.noreply.github.com>
2025-04-18 10:41:08 -0400

    fix: pass uids

--- src/handlers/disassembleXMLFileHandler.ts
+++ src/handlers/disassembleXMLFileHandler.ts
@@ -78,6 +78,7 @@ export class DisassembleXMLFileHandler {
             dirPath: filePath,
             strategy,
             filePath: subFilePath,
+            uniqueIdElements,
             prePurge,
             postPurge,
             format,
